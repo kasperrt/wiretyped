ff2ec21079bf2f669af3bdd73e9b85652fca6241
Kasper Rynning-Tønnesen <kasper@rynning-toennesen.no>
2025-12-06 22:20:48 +0100

    feat: e2e tests

---
+++ e2e/vitest.e2e.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['e2e/**/*.test.ts'],
+    testTimeout: 10_000,
+    hookTimeout: 10_000,
+    environment: 'node',
+  },
+});
